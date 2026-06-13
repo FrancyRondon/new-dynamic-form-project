3e4db4a8a036edbcc44f0ef23c6d803a3bb76782
Francy Rondon <cranfhuo@gmail.com>
2026-06-13 15:20:01 -0400

    feat: add steps

--- src/components/DynamicForm/DynamicForm.js
+++ src/components/DynamicForm/DynamicForm.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { Container } from "react-bootstrap";
 import "./DynamicForm.css";
+import FirstStep from "./FirstStep/FirstStep";
+import SecondStep from "./SecondStep/SecondStep";
+import ThirdStep from "./ThirdStep/ThirdStep";
 
 export default function DynamicForm() {
     const [step, setStep] = useState(1);
@@ -18,21 +21,41 @@ export default function DynamicForm() {
       <Container className="text-center p-4">
         <div className="content-dynamic-form mt-5 p-4">
           <h1 className="title-dynamic-form">Fill the form</h1>
-          <p className="steps-numbers">Step {step} of 3</p>          
-        </div>
-        <div className="actions">
+          <p className="steps-numbers">Step {step} of 3</p> 
+
+          {getPageByStep(step, setStep)}
+
+           <div className="actions">
             <div className="container-text">
+              {step > 1 && step < 4 && (
                 <p className="before" onClick={showPreviewStep}>
-                  ⬅️ Before Step
+                                  ⬅️ Before Step
                 </p>
+              )}
+                
             </div>
             <div className="container-text">
+              {step > 0 && step < 3 && (
                 <p className="next" onClick={showNextStep}>
                   Next Step ➡️
                 </p>
+              )}
+                
             </div>
-          </div>
+          </div>         
+        </div>
       </Container>
     </div>
   );
-};
+}
+
+function getPageByStep(step, setStep) {
+  if (step === 1) {
+    return <FirstStep/>;
+  } else if (step === 2) {
+    return <SecondStep/>;
+  } else if (step === 3) {
+    return <ThirdStep/>;
+  }
+}
+
